8c62f03ea79f9fc8230b616fbacbaca70a4d6141
cielavenir <cielartisan@gmail.com>
2017-01-08 21:27:31 +0000

    update script

--- openrave/tests.py
+++ openrave/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class RobotTest(TestCase):
+	def test_add(self):
+		response=self.client.post('/add/test',{'file':''})
+		self.failUnlessEqual(response.status_code,200)
+	def test_remove(self):
+		pass
+
--- script.py
+++ script.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python
 import sys,httplib,urllib
-if len(sys.argv)<4:
-	sys.stderr.write("script host[:port] robot_name [filepath|--remove]")
-	sys.exit(1)
+from contextlib import closing
 
-robot_name=sys.argv[2]
-robot_file=sys.argv[3]
-if robot_file!='--remove':
-	with open(robot_file) as f:
-		robot_file=f.read()
+def add_robot(host,robot_name,robot_content):
+	with closing(httplib.HTTPConnection(host)) as http:
+		http.request('POST','/add/'+robot_name,urllib.urlencode({'file':robot_content}),{'Content-type':"application/x-www-form-urlencoded"})
+		return http.getresponse().read()
 
-http=httplib.HTTPConnection(sys.argv[1])
-if robot_file=='--remove':
-	http.request('GET','/remove/'+robot_name)
-else:
-	http.request('POST','/add/'+robot_name,urllib.urlencode({'file':robot_file}),{'Content-type':"application/x-www-form-urlencoded"})
+def remove_robot(host,robot_name):
+	with closing(httplib.HTTPConnection(host)) as http:
+		http.request('GET','/remove/'+robot_name)
+		return http.getresponse().read()
 
-sys.stdout.write(http.getresponse().read())
+if __name__=='__main__':
+	if len(sys.argv)<4:
+		sys.stderr.write("script host[:port] robot_name [filepath|--remove]\n")
+		sys.exit(1)
+	host=sys.argv[1]
+	robot_name=sys.argv[2]
+	robot_file=sys.argv[3]
+	if robot_file!='--remove':
+		with open(robot_file) as f:
+			robot_file=f.read()
+	http=httplib.HTTPConnection(sys.argv[1])
+	if robot_file=='--remove':
+		sys.stdout.write(remove_robot(host,robot_name))
+	else:
+		sys.stdout.write(add_robot(host,robot_name,robot_file))
