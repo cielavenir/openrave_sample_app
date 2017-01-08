71a2a0f22dfef6a67b43c669adba6b38bec0bd17
cielavenir <cielartisan@gmail.com>
2017-01-08 13:47:44 +0000

    update app

--- openrave/models.py
+++ openrave/models.py
@@ -2,5 +2,7 @@ from django.db import models
 class Robot(models.Model):
 	name = models.CharField(max_length=200)
 	file = models.FileField(upload_to='robots')
-
-# Create your models here.
+	@classmethod
+	def create(klass,name,file):
+		robot = klass(name=name,file=file)
+		return robot
--- openrave/views.py
+++ openrave/views.py
@@ -1,14 +1,39 @@
 from openrave.models import Robot
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+import openravepy
+import StringIO
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+	resp=StringIO.StringIO()
+	robots=Robot.objects.all()
+	env=openravepy.Environment()
+	with env:
+		resp.write("<html><body>")
+		resp.write("%d robots<br>"%len(robots))
+		for e in robots:
+			env.LoadData(e.file)
+			resp.write("[%s] jobs=%d<br>"%(e.name,env.GetRobots()[-1].GetDOF()))
+		resp.write("</body></html>")
+	env.Destroy()
+	return HttpResponse(resp)
 
 def add(request,robot_name=''):
-    return HttpResponse("add [%s] not implemented yet"%robot_name)
+	if not robot_name:
+		return HttpResponse("robot_name is empty")
+	if any(e.name==robot_name for e in Robot.objects.all()):
+		return HttpResponse("robot_name %s already exists")
+	try:
+		robot_file=request.GET['file']
+		q=Robot.create(robot_name,robot_file)
+		return HttpResponse("added [%s]"%robot_file)
+	except KeyError:
+		return HttpResponse("file param not specified")
 
 def remove(request,robot_name=''):
-    return HttpResponse("remove [%s] not implemented yet"%robot_name)
+	q=get_object_or_404(Robot,name=robot_name)
+	q.delete()
+	return HttpResponse("removed [%s]"%robot_name)
 
