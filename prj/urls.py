2de06fb0a659ab2957475210834614ecc3a199a7
cielavenir <cielartisan@gmail.com>
2017-08-04 10:49:45 +0900

    Update for django 1.10

--- prj/urls.py
+++ prj/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
-
-urlpatterns = patterns('',
+from openrave import views as openrave_views
+urlpatterns = [
     # Examples:
-    url(r'^$', 'openrave.views.index'),
-    url(r'^info.json$', 'openrave.views.info'),
-    url(r'^add/(?P<robot_name>\w*)$', 'openrave.views.add'),
-    url(r'^remove/(?P<robot_name>\w*)$', 'openrave.views.remove'),
+    url(r'^$', openrave_views.index),
+    url(r'^info.json$', openrave_views.info),
+    url(r'^add/(?P<robot_name>\w*)$', openrave_views.add),
+    url(r'^remove/(?P<robot_name>\w*)$', openrave_views.remove),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
